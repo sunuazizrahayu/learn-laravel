dde31d8801038e9fbd36230a4b17cef175c884f2
Sunu <sunuazizrahayu@gmail.com>
2020-05-11 23:31:16 +0700

    intro - instalasi laravel - routing - blade templating - send data ke view

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+	$data = [
+		'name' => 'Sunu Aziz'
+	];
+    return view('about', $data);
 });
